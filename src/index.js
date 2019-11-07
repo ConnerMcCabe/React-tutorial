e1a0931fabe4490f99c4636951aa34eae9aa2aa8
Admin <conner.p.mccabe@gmail.com>
2019-11-07 15:21:15 -0600

    final tweaks

--- src/index.js
+++ src/index.js
@@ -103,15 +103,16 @@ class Game extends React.Component {
         }
       return (
         <div className="game">
+        <h2 className="bill-board">Tic Tac Toe</h2>
           <div className="game-board">
             <Board 
                 squares={current.squares}
                 onClick={(i) => this.handleClick(i)}
             />
-          </div>
           <div className="game-info">
             <div>{status}</div>
             <ol>{moves}</ol>
+          </div>
           </div>
         </div>
       );
